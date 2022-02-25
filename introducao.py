e89eaf7a23c09cc169f5f632e028ca5c2ed5ba2a
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-02-24 21:48:49 -0300

    first commit

---
+++ introducao.py
@@ -0,0 +1,13 @@
+"""
+Paradigma Imperativo - Fortran - Sequência, Decisão e Iteração
+Paradigma Estruturado - C - Structs
+Paradigma Procedural - Python
+"""
+
+
+def Registrar(nome, idade, cpf, email):
+    paciente = {'nome': nome, 'idade': idade, 'cpf': cpf, 'email': email}
+    return paciente
+
+
+# Reuso e Coesão: sempre focar nisso
