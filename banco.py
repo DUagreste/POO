894e129c954d2fd6fa562464816630a7f9d4cf3f
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-03-03 19:57:50 -0300

    Atualização: aulas sobre Atributos.

--- banco.py
+++ banco.py
@@ -1,11 +1,16 @@
 class Conta:
 
+    # Atributos de classe
+    taxa = 0.50
+
+    # Atributos de instâncias
     def __init__(self, numero, titular, saldo):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
 
     def extrato(self):
+        self.saldo -= Conta.taxa
         print(f'Saldo: R${self.saldo}')
 
     def deposito(self, valor):
@@ -15,3 +20,24 @@ class Conta:
     def saque(self, valor):
         self.saldo -= valor
         print("Transação efetuada com sucesso!")
+
+
+# Instâncias da Classe Conta
+conta1 = Conta(1, 'Maria Silva', 5000)
+conta2 = Conta(2, 'Paulo Santos', 2000)
+
+print(conta1.__dict__)
+print(conta2.__dict__)
+
+conta1.extrato()
+
+# Atributo Dinâmico -> Criado na execução
+conta2.carro = 'fiat uno'
+
+print(conta1.__dict__)
+print(conta2.__dict__)
+
+del conta2.carro
+
+print(conta1.__dict__)
+print(conta2.__dict__)
