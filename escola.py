6782934a8b929a8270b044306699457c5d03a346
Vítor Pinheiro <vitor.pinheiro97@hotmail.com>
2022-03-22 20:50:07 -0300

    Aulas: set & get e decorators.

--- escola.py
+++ escola.py
@@ -1,14 +1,30 @@
 class Aluno:
 
     def __init__(self, nome, idade, matricula):
-        self.nome = nome
-        self.idade = idade
+        self.__nome = nome
+        self._idade = idade
         self.matricula = matricula
         self.notas = None
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.setter
+    def idade(self, idade):
+        self._idade = idade
 
 
 aluno1 = Aluno("Vitor", 22, 123456)
 print(aluno1.nome)
+aluno1.nome = "João"
 print(aluno1.idade)
-print(aluno1.matricula)
-print(aluno1.notas)
+print(aluno1.nome)
